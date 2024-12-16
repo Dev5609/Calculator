8a61d87ee4cd249892621307ae64242635bff43f
Dev5609 <devanshg5609@gmail.com>
2024-12-16 19:22:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
-        if (buttonText == '*') {
+        if (buttonText == 'X') {
             buttonText = '*';
             screenValue += buttonText;
             screen.value = screenValue;
@@ -14,11 +14,15 @@ for (item of buttons) {
             screen.value = screenValue;
         } else if (buttonText == '=') {
             screen.value = eval(screenValue);
-        } else {
+        } else if(buttonText == "÷"){
+            buttonText = '/';
             screenValue += buttonText;
             screen.value = screenValue;
         }
-
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
 
     })
 }
